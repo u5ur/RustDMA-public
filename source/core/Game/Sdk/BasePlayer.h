6cc2c91222557e04fc7bf96bdfba0a58ab6e6eef
no <wunjacob@gmail.com>
2025-04-23 10:52:38 -0400

    added playerlist offsets

--- source/core/Game/Sdk/BasePlayer.h
+++ source/core/Game/Sdk/BasePlayer.h
@@ -10,13 +10,13 @@ public:
 
 	static uint64_t GetPlayerList()
 	{
-		uint64_t TypeInfo = mem.Read<uint64_t>(mem.BaseAddress + 198117072);
+		uint64_t TypeInfo = mem.Read<uint64_t>(mem.BaseAddress + offsets::BasePlayer);
         if (!TypeInfo) return 0;
 
 		uint64_t StaticFields = mem.Read<uint64_t>(TypeInfo + 0xb8);
         if (!StaticFields) return 0;
 
-        auto Dict = Decrypt::BaseNetworkable1(mem.Read<uint64_t>(StaticFields + 0x13c0));
+        auto Dict = Decrypt::BaseNetworkable1(mem.Read<uint64_t>(StaticFields + offsets::PlayerList));
         if (!Dict) return 0;
 
         return mem.Read<uint64_t>(Dict + 0x18);
